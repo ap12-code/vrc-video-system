2a2cb5a206bb4ca6ad2f7c8b3d3caee8de0ec209
黒猫ちゃん <42853476+kuroneko6423@users.noreply.github.com>
2023-07-03 19:30:55 +0900

    add:404pages

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
-const express = require("express")()
-const ytdl = require("ytdl-core")
+const express = require("express");
+const ytdl = require("ytdl-core");
 
-express.get("/proxy", (req, res) => {
-    let data = req.query.url
-    console.log(req.headers["user-agent"])
+const app = express();
+const port = 12321;
+
+app.get("/proxy", (req, res) => {
+    let data = req.query.url;
+    console.log(req.headers["user-agent"]);
     if (req.headers["user-agent"].includes("Mozilla")) {
-        res.status(302).redirect(data)
+        res.status(302).redirect(data);
     } else {
         try {
-            ytdl(data, {filter: (p => p.hasAudio == true && p.hasVideo == true), liveBuffer: 50000}).pipe(res)
+            ytdl(data, {
+                filter: (p => p.hasAudio == true && p.hasVideo == true),
+                liveBuffer: 50000
+            }).pipe(res);
         } catch {
-            console.error("err")
+            console.error("err");
+            res.status(404).sendFile(__dirname + "/pages/404.html");
         }
     }
-})
-express.get("/", (req, res) => {
-    res.sendFile(__dirname + "/pages/index.html")
 });
 
-express.listen(12321)
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/pages/index.html");
+});
+
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname + "/pages/404.html");
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
